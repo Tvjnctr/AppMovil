af3254eaf5993ee8f4e981f50f689a830dcad041
GonzaSV <gon.sepulveda@duocuc.cl>
2023-11-23 15:15:22 -0300

    generar qr

--- 01_counter_app/src/app/qr-generado/qr-generado.module.ts
+++ 01_counter_app/src/app/qr-generado/qr-generado.module.ts
@@ -8,8 +8,11 @@ import { QrGeneradoPageRoutingModule } from './qr-generado-routing.module';
 
 import { QrGeneradoPage } from './qr-generado.page';
 
+import { QRCodeModule } from 'angularx-qrcode';
+
 @NgModule({
   imports: [
+    QRCodeModule,
     CommonModule,
     FormsModule,
     IonicModule,
